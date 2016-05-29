933442ca8bbef4cd966bc772138139f2e973ec56
rafaellucio <rafaelantoniolucio@gmail.com>
2016-05-28 21:06:16 -0300

    incluido try/catch como responsabilidade da classe que dispara o tweete

--- src/br/com/app/twitter/TwitterSendTweet.java
+++ src/br/com/app/twitter/TwitterSendTweet.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.conf.Configuration;
@@ -16,7 +17,7 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterSendTweet {
 	
-	public static void sendMessage(String message) throws Exception {
+	public static void sendMessage(String message) {
 		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 		
 		configurationBuilder
@@ -31,9 +32,15 @@ public class TwitterSendTweet {
 		AccessToken accessToken = accessToken();
 		
 		twitter.setOAuthAccessToken(accessToken);
-		Status status = twitter.updateStatus(message);
+		Status status;
 		
-		JOptionPane.showMessageDialog(null, "Tweet postado com sucesso! [" + status.getText() + "]");
+		try {
+			status = twitter.updateStatus(message);
+			JOptionPane.showMessageDialog(null, "Tweet postado com sucesso! [" + status.getText() + "]");
+		} catch (TwitterException e) {
+			JOptionPane.showMessageDialog(null, "Não foi possível enviar o tweet");
+			e.printStackTrace();
+		}
 	}
 	
 	public static AccessToken accessToken() {
